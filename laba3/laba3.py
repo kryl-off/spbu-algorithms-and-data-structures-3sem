4787aab7adab525e0c02a613957016f78a8e20fc
islanditurup <sandy1bridge@gmail.com>
2024-10-22 06:51:05 +0300

    start laba3

--- laba3/laba3.py
+++ laba3/laba3.py
@@ -1,16 +1,33 @@
-import itertools
-import string
 
+#hashcat -m 0 -a 3 -o cracked_hashes.txt hashes.txt 89\?d\?d\?d\?d\?d\?d\?d\?d\?d --potfile-disable
+#получаем расшифрованные первично номера
 
+f = open("../data/laba3/phones.txt")
+phones = []
+for i in f.read().split():
+    phones.append(int(i))
 
-def clean_wordlist(input_file, output_file):
-    with open(input_file, 'r', encoding='utf-16') as infile, open(output_file, 'w', encoding='utf-8') as outfile:
-        for line in infile:
-            # Удаляем лишние пробелы и не отображаемые символы
-            cleaned_line = line.strip()  # Убираем пробелы и символы новой строки в начале и конце
-            if cleaned_line:  # Если строка не пустая после очистки
-                outfile.write(cleaned_line + '\n')  # Записываем её в файл
+phones_set = set(phones)
 
-# Пример использования
-clean_wordlist('../data/laba3/hashes.txt', '../data/laba3/clean_hashes.txt')
-print("Очистка завершена.")
+phones_with_salt_a = open("../data/laba3/cracked_hashes.txt").read().split()
+phones_with_salt = [int(i[-11:]) for i in phones_with_salt_a]
+
+for k in phones_with_salt:
+    counter = 0
+    salt = k-phones[4]
+    for v in phones_with_salt:
+        if v-salt in phones_set:
+            counter +=1
+    if counter == len(phones):
+        break
+print(salt)
+
+#87893892
+
+phones_wo_salt = [x - salt for x in phones_with_salt]
+with open("../data/laba3/phones_out.txt", 'w') as file:
+    lines = []
+    for i in range (len(phones_with_salt_a)):
+        i_sliced = str(phones_with_salt_a[i])[0:-11]
+        lines.append(f"{i_sliced}{phones_wo_salt[i]}\n")
+    file.writelines(lines)
